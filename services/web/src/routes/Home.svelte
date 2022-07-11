073ad08b344640f1afe40e3b905d872c386533e0
lebrancconvas <lebrancconvas@gmail.com>
2022-07-11 20:38:58 +0700

    ✨ Create Homepage.

--- services/web/src/routes/Home.svelte
+++ services/web/src/routes/Home.svelte
@@ -1,3 +1,31 @@
+<script lang="ts">
+import { onMount } from "svelte";
+import axios from 'axios';
+
+let quizes = [];
+
+onMount(async () => {
+	const res = await axios.get('http://localhost:9000/quiz');
+	quizes = res.data;
+});
+</script>
+
 <main>
-	<h1>Hello</h1>
+	<header>
+		<div>
+			<h1>Question</h1>
+		</div>
+	</header>
+	<section class="quiz">
+		{#each quizes as quiz}
+		<div>
+			<h2>{quiz.question}</h2>
+			<div class="choice">
+				{#each quiz.choices as choice}
+					<div>{choice}</div>
+				{/each}
+			</div>
+		</div>
+		{/each}
+	</section>
 </main>
